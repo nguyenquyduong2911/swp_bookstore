3b4db7701d6dde99f9d898dcc9c36bc7834445e8
thongndhe170758 <thongndhe170758@fpt.edu.vn>
2023-06-06 16:12:25 +0700

    them ti comment

--- src/java/controller/LoginServlet.java
+++ src/java/controller/LoginServlet.java
@@ -30,7 +30,8 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html;charset=UTF-8"); //hello
+        
         String email, pass;
         email = request.getParameter("email");
         pass = request.getParameter("pass");
