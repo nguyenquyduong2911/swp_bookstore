eb0720560dad709c4443cb238bf83fd7b57a570f
Nguyen Quy Duong <duongnqhe170786@fpt.edu.vn>
2023-06-25 20:33:38 +0700

    checkout button change logic redirect

--- src/java/controller/validateCheckout.java
+++ src/java/controller/validateCheckout.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dal.OrderDAO;
@@ -24,36 +23,39 @@ import java.util.logging.Logger;
  *
  * @author taote
  */
-@WebServlet(name="validateCheckout", urlPatterns={"/validate"})
+@WebServlet(name = "validateCheckout", urlPatterns = {"/validate"})
 public class validateCheckout extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet validateCheckout</title>");  
+            out.println("<title>Servlet validateCheckout</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet validateCheckout at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet validateCheckout at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -61,27 +63,33 @@ public class validateCheckout extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-             HttpSession session = request.getSession(true);
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
         Cart cart = null;
         Object o = session.getAttribute("cart");
         //co roi\
-        if(o!=null){
-            cart = (Cart)o;
-        }else{
+        if (o != null) {
+            cart = (Cart) o;
+        } else {
             cart = new Cart();
         }
-         Account ac = null;
-          Object a = session.getAttribute("curr");
-          if(a!=null){
-              response.sendRedirect("checkout.jsp");
-          }else{
-              response.sendRedirect("login.jsp");
-          }
-    } 
+        Account ac = null;
+        Object a = session.getAttribute("curr");
 
-    /** 
+        if (a != null) {
+            if (cart.getItem().isEmpty()) {
+                response.sendRedirect("cart.jsp");
+            } else {
+                response.sendRedirect("checkout.jsp");
+            }
+        } else {
+            response.sendRedirect("login.jsp");
+        }
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -89,28 +97,29 @@ public class validateCheckout extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-               HttpSession session = request.getSession(true);
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
         Cart cart = null;
         Object o = session.getAttribute("cart");
         //co roi\
-        if(o!=null){
-            cart = (Cart)o;
-        }else{
+        if (o != null) {
+            cart = (Cart) o;
+        } else {
             cart = new Cart();
         }
-         Account ac = null;
-          Object a = session.getAttribute("curr");
-          if(a!=null){
-              response.sendRedirect("checkout.jsp");
-          }else{
-              response.sendRedirect("login.jsp");
-          }
-    
+        Account ac = null;
+        Object a = session.getAttribute("curr");
+        if (a != null) {
+            response.sendRedirect("checkout.jsp");
+        } else {
+            response.sendRedirect("login.jsp");
+        }
+
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
